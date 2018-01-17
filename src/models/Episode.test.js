683227c52bebdbe6c90631f3f1fd47e42ffea3cd
Raul Melo <melo.raulf@gmail.com>
2018-01-17 23:48:38 +0100

    Fix all unit tests and, added a githook to avoid push with erros and separate test command for lint

--- src/models/Episode.test.js
+++ src/models/Episode.test.js
@@ -1,11 +1,10 @@
 const Episode = require('./Episode')
-const name = 'Episode 1'
 const link = '/tv/game-of-thrones/season-1/episode-1/'
-const episode = new Episode(name, link)
+const episode = new Episode(1, link)
 
 describe('Episode Class', () => {
   test('should return correct name', () => {
-    expect(episode.name).toBe(name)
+    expect(episode.name).toBe('Episode 1')
   })
   test('should return full URL', () => {
     expect(episode.link).toBe('https://www.tv-subs.com' + link)
--- src/models/Season.test.js
+++ src/models/Season.test.js
@@ -1,11 +1,10 @@
 const Season = require('./Season')
-const name = 'Season 1'
 const link = '/tv/game-of-silence/season-1/'
-const season = new Season(name, link)
+const season = new Season(1, link)
 
 describe('Season Class', () => {
   test('should return correct name', () => {
-    expect(season.name).toBe(name)
+    expect(season.name).toBe('Season 1')
   })
   test('should return full URL', () => {
     expect(season.link).toBe('https://www.tv-subs.com' + link)
