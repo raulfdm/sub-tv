b35222d70e494f87ae884830007ae5c46d4cbfa4
Raul Melo <melo.raulf@gmail.com>
2018-01-17 22:23:35 +0100

    Fixed link of the new API

--- src/service/Serie.js
+++ src/service/Serie.js
@@ -1,13 +1,13 @@
 const fetch = require('node-fetch')
 const Serie = require('../models/Serie')
 
-const fetchSeries = serieName => {
-  const url = `https://www.tv-subs.com/a_mov.php?reqmov=${serieName}`
+const fetchSeries = (serieName = '""') => {
+  const url = `https://www.tv-subs.com/ajax_search.php?mov=${serieName}`
   const series = _searchSeries(url)
 
-  return series.then(series =>
-    series.movies.map(serie => new Serie(serie.label, serie.value))
-  )
+  return series.then(series => {
+    return series.map(serie => new Serie(serie.tv, serie.slug))
+  })
 }
 
 const _searchSeries = url => {
