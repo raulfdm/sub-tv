7323720125da7f6b1c658429dfea67f7adec581c
Raul de Melo <melo.raulf@gmail.com>
2018-12-30 14:27:58 +0100

    Create barrel for services

---
+++ src/service/index.js
@@ -0,0 +1,5 @@
+export { default as DownloadService } from './Download';
+export { default as EpisodeService } from './Episode';
+export { default as SeasonService } from './Season';
+export { default as SeriesService } from './Series';
+export { default as SubtitleService } from './Subtitle';
