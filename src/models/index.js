a778aaab7fd8412941d63c4fad1355006d82c6a2
Raul de Melo <melo.raulf@gmail.com>
2018-12-30 14:24:30 +0100

    Add barrel to models

---
+++ src/models/index.js
@@ -0,0 +1,5 @@
+export { default as EpisodeModel } from './Episode';
+export { default as SeasonModel } from './Season';
+export { default as SeriesModel } from './Series';
+export { default as SubtitleListModel } from './SubtitleList';
+export { default as SubtitleModel } from './Subtitle';
