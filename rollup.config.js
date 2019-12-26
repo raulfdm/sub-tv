db0677c34dd45f2c45aa0cc69e739283e52b4228
Raul de Melo <melo.raulf@gmail.com>
2019-12-26 12:01:22 +0100

    build: add TS + rollup to compile files

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import builtins from 'rollup-plugin-node-builtins';
+
+import pkg from './package.json';
+
+export default {
+  input: './src/index.js',
+  output: {
+    file: 'dist/sub-tv.js',
+    format: 'cjs',
+    banner: '#!/usr/bin/env node',
+  },
+
+  external: [].concat(Object.keys(pkg.dependencies)),
+
+  plugins: [typescript(), json(), resolve(), commonjs({ exclude: ['node_modules'] }), builtins()],
+};
