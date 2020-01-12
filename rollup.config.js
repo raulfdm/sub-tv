3ba1499a7bb864ef1528caf9b9a8b6509b7e55f9
Raul de Melo <melo.raulf@gmail.com>
2020-01-12 18:26:33 +0100

    refactor: add eslint and clean up project files

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,43 @@
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
+import replace from '@rollup/plugin-replace';
 import resolve from '@rollup/plugin-node-resolve';
 import builtins from 'rollup-plugin-node-builtins';
+import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
 
 export default {
-  input: './src/index.ts',
+  input: './src/app.ts',
   output: {
     file: 'dist/sub-tv.js',
     format: 'cjs',
     banner: '#!/usr/bin/env node',
   },
 
-  external: [].concat(Object.keys(pkg.dependencies)),
+  external: Object.keys(pkg.dependencies),
 
   plugins: [
-    typescript(),
+    typescript({
+      inlineSourceMap: true,
+    }),
     json(),
     resolve({
       preferBuiltins: true,
     }),
-    commonjs({ exclude: ['node_modules'] }),
+    commonjs({ sourceMap: true, exclude: ['node_modules'] }),
     builtins(),
+    terser({
+      sourcemap: true,
+      output: {
+        comments: false,
+      },
+    }),
+    replace({
+      __SERVER_URL__: process.env.ROLLUP_WATCH
+        ? 'http://localhost:3000'
+        : 'http://service.subtvapi.app',
+    }),
   ],
 };
