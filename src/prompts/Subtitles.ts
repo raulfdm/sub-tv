c4e6fd318286a791970aae3b552a76b2a25c86dd
Raul de Melo <melo.raulf@gmail.com>
2020-01-13 21:06:40 +0100

    fix: add validation to prevent user go ahead without select any subtitle

--- src/prompts/Subtitles.ts
+++ src/prompts/Subtitles.ts
@@ -13,11 +13,19 @@ export async function SubtitlesPrompt(): Promise<unknown> {
     return subtitles;
   }
 
+  function validate(userInput: string[]): string | boolean {
+    if (!userInput || userInput.length < 1) {
+      return 'You must select at least one subtitle (use your space bar to do it)';
+    }
+    return true;
+  }
+
   return new PromptFactory<PromptCheckboxQuestion>({
     choices,
     name: 'subtitle',
     message: 'Choose all subtitles you want to download',
     filter,
     type: 'checkbox',
+    validate,
   }).ask();
 }
