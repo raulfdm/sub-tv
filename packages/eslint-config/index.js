51000e4fa8459a92deda60bec0398cbfcf38e495
Raul Melo <melo.raulf@gmail.com>
2022-08-29 07:42:13 +0200

    chore: enhance eslint config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,11 +1,17 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "jest"],
+  plugins: ["@typescript-eslint", "simple-import-sort"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier"
   ],
+  rules: {
+    "@typescript-eslint/consistent-type-assertions": 'error',
+    "@typescript-eslint/consistent-type-imports": 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  }
 };
