a5442a23e062d920120d1fbaf8432f8494993abe
Raul Melo <melo.raulf@gmail.com>
2022-08-30 15:31:19 +0200

    fix: dev command

--- packages/open-subtitle/vite.config.ts
+++ packages/open-subtitle/vite.config.ts
@@ -1,20 +1,32 @@
-import { isAbsolute, resolve } from "path";
-import { defineConfig } from "vite";
+import { isAbsolute, resolve } from 'path';
+import { defineConfig } from 'vite';
 
 function isExternal(id: string): boolean {
-  return !id.startsWith(".") && !isAbsolute(id) && !id.startsWith("~/");
+  return !id.startsWith('.') && !isAbsolute(id) && !id.startsWith('~/');
 }
 
-export default defineConfig({
-  build: {
-    minify: false,
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      fileName: "index",
-      formats: ["es"]
+export default defineConfig(({ mode, command }) => {
+  /**
+   * In `build --watch` (for libs), mode is always set to production and
+   * command is set to `build`.
+   *
+   * I had to use a custom environment variable to know if I'm running dev command
+   * or building for production
+   */
+  const isDevMode = process.env.DEV_MODE === 'true';
+
+  return {
+    build: {
+      minify: isDevMode === false,
+      emptyOutDir: isDevMode === false,
+      lib: {
+        entry: resolve(__dirname, 'src/index.ts'),
+        fileName: 'index',
+        formats: ['es'],
+      },
+      rollupOptions: {
+        external: isExternal,
+      },
     },
-    rollupOptions: {
-      external: isExternal
-    }
-  }
+  };
 });
