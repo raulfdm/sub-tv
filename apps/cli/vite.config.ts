5c1effe375b6fc957913c28eb59534d6ddd5a859
Raul Melo <melo.raulf@gmail.com>
2022-08-30 06:56:12 +0200

    chore: setup run commands

--- apps/cli/vite.config.ts
+++ apps/cli/vite.config.ts
@@ -1,23 +1,20 @@
-import { resolve, isAbsolute } from "path";
-import { defineConfig } from "vite";
+import { isAbsolute, resolve } from 'path';
+import { defineConfig } from 'vite';
 
 function isExternal(id: string) {
-  return !id.startsWith(".") && !isAbsolute(id) && !id.startsWith("~/");
+  return !id.startsWith('.') && !isAbsolute(id) && !id.startsWith('~/');
 }
 
 export default defineConfig({
-  define: {
-    __SERVER_URL__: process.env.SERVER_URL || "http://localhost:5001"
-  },
   build: {
     minify: false,
     lib: {
-      entry: resolve(__dirname, "src/app.ts"),
-      fileName: "sub-tv",
-      formats: ["es"]
+      entry: resolve(__dirname, 'src/main.ts'),
+      fileName: 'sub-tv',
+      formats: ['es'],
     },
     rollupOptions: {
-      external: isExternal
-    }
-  }
+      external: isExternal,
+    },
+  },
 });
