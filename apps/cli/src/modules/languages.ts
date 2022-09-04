35b0c44963d5b303c4fd8cdee07bb66eb48f8083
Raul Melo <melo.raulf@gmail.com>
2022-09-04 18:37:43 +0200

    refactor: language choices

--- apps/cli/src/modules/languages.ts
+++ apps/cli/src/modules/languages.ts
@@ -1,24 +1,113 @@
+import type { OpenSubtitleLanguagesApiResponse } from '@sub-tv/open-subtitle';
 import prompts from 'prompts';
+import { assign, createMachine, interpret } from 'xstate';
 
 import { apiClient } from '../config/apiClient';
 import { db } from '../config/db';
 
+const languagesMachine = createMachine(
+  {
+    id: 'languages',
+    initial: 'languages',
+    schema: {
+      context: {} as {
+        allLanguages: OpenSubtitleLanguagesApiResponse['data'];
+        selectedLanguage: string[];
+        initialLanguages: string[];
+      },
+    },
+    on: {
+      CANCEL: {
+        target: 'finish',
+      },
+    },
+    states: {
+      languages: {
+        invoke: {
+          src: async (context) => {
+            return prompts({
+              type: 'autocompleteMultiselect',
+              name: 'languages',
+              message: 'Select the languages you want to search subtitles for:',
+              choices: context.allLanguages.map((lang) => ({
+                value: lang.language_code,
+                title: lang.language_name,
+                selected: context.initialLanguages.includes(lang.language_code),
+              })),
+            });
+          },
+          onDone: [
+            {
+              target: 'finish',
+              cond: (_, event) => event.data.languages === undefined,
+            },
+            {
+              target: 'confirm',
+              cond: (_, event) => event.data.languages.length > 0,
+              actions: ['saveSelectedLanguages'],
+            },
+            {
+              target: 'languages',
+            },
+          ],
+        },
+      },
+      confirm: {
+        invoke: {
+          src: async (context) => {
+            return prompts({
+              type: 'confirm',
+              name: 'confirm',
+              initial: true,
+              message: `You selected the following languages: "${context.selectedLanguage.join(
+                ', ',
+              )}". Is this correct?`,
+            });
+          },
+          onDone: [
+            {
+              target: 'finish',
+              actions: ['saveLanguages'],
+              cond: (_, event) => {
+                console.log('event.data', event.data);
+                return event.data.confirm === true;
+              },
+            },
+            {
+              target: 'languages',
+            },
+          ],
+        },
+      },
+      finish: {
+        type: 'final',
+      },
+    },
+  },
+  {
+    actions: {
+      saveSelectedLanguages: assign({
+        selectedLanguage: (context, event) => event.data.languages || context.initialLanguages,
+      }),
+      saveLanguages: (context) => {
+        db.setLangs(context.selectedLanguage);
+      },
+    },
+  },
+);
+
 export async function languagesPrompt() {
   const allLanguages = await apiClient.fetchLanguages();
 
-  const { languages } = await prompts([
-    {
-      type: 'autocompleteMultiselect',
-      name: 'languages',
-      message: 'Select the languages you want to search subtitles for:',
-      choices: allLanguages.map((lang) => ({
-        value: lang.language_code,
-        title: lang.language_name,
-        selected: db.preferredLanguages.includes(lang.language_code),
-      })),
-    },
-  ]);
+  return new Promise((resolve) => {
+    const service = interpret(
+      languagesMachine.withContext({
+        initialLanguages: db.preferredLanguages,
+        selectedLanguage: [],
+        allLanguages,
+      }),
+    ).start();
 
-  // TODO: move this to an action
-  db.setLangs(languages as string[]);
+    service.onDone(resolve);
+  });
 }
