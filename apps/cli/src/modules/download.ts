f65aec7376f921517c2955d0827290d7ca892c2e
Raul Melo <melo.raulf@gmail.com>
2022-09-04 18:37:00 +0200

    chore: remove unused stuff

--- apps/cli/src/modules/download.ts
+++ apps/cli/src/modules/download.ts
@@ -5,32 +5,26 @@ import path from 'path';
 
 import { apiClient } from '../config/apiClient';
 import { db } from '../config/db';
-import { inquirerUi } from '../config/inquirer';
 import type { SubTvMachineContext } from '../types/main';
 
 export async function downloadSubtitles(context: SubTvMachineContext): Promise<{
   remainingDownloads: OpenSubtitleDownloadApiResponse['remaining'] | null;
 }> {
   let remainingDownloads: number | null = null;
-  const downloadedSubtitlesIds = new Map();
-  const errorSubtitles = new Map();
 
-  const downloads = context.subtitlesIdToDownload.map(async (subtitleId) => {
-    try {
-      const downloadInfo = await apiClient.download(subtitleId);
-      await download(downloadInfo.link, downloadInfo.file_name);
-      remainingDownloads = downloadInfo.remaining;
-      db.setDownloads(downloadInfo);
-      downloadedSubtitlesIds.set(subtitleId, downloadInfo);
-    } catch (error: any) {
-      errorSubtitles.set(subtitleId, error);
-      console.error(error.message);
-    }
-  });
+  await Promise.all(
+    context.subtitlesIdToDownload.map(async (subtitleId) => {
+      try {
+        const downloadInfo = await apiClient.download(subtitleId);
 
-  await Promise.all(downloads);
-
-  inquirerUi.updateBottomBar(`All subtitles downloaded. Remaining downloads: ${remainingDownloads}\n\n`);
+        await download(downloadInfo.link, downloadInfo.file_name);
+        remainingDownloads = downloadInfo.remaining;
+        db.setDownloads(downloadInfo);
+      } catch (error: any) {
+        console.error(error.message);
+      }
+    }),
+  );
 
   return {
     remainingDownloads,
