5fef370705dbbb1cc3d321655c2dacda65a3f40a
Raul Melo <melo.raulf@gmail.com>
2022-09-04 18:36:46 +0200

    refactor: getAppTitle function

--- apps/cli/src/modules/welcome.ts
+++ apps/cli/src/modules/welcome.ts
@@ -1,12 +1,5 @@
 import figlet from 'figlet';
 
-import { inquirerUi } from '../config/inquirer';
-
-function showAppTitle(): void {
-  inquirerUi.log.write(figlet.textSync('Sub - TV'));
-}
-
-export function clearConsoleWithAppTitle(): void {
-  // console.clear();
-  showAppTitle();
+export function getAppTitle(): string {
+  return figlet.textSync('Sub - TV');
 }
