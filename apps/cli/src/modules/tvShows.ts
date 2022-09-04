02765e864f07476e052b185910c31aac53dfe226
Raul Melo <melo.raulf@gmail.com>
2022-09-04 17:21:26 +0200

    refactor: tv shows to use prompts

--- apps/cli/src/modules/tvShows.ts
+++ apps/cli/src/modules/tvShows.ts
@@ -1,32 +1,32 @@
-import type { Feature } from '@sub-tv/open-subtitle';
-import inquirer from 'inquirer';
+import type { Choice } from 'prompts';
+import prompts from 'prompts';
 
-import { createPromptModule } from '../config/inquirer';
 import type { SubTvMachineContext } from '../types/main';
 
 export async function tvShowPrompt(context: SubTvMachineContext): Promise<string[]> {
-  const prompt = createPromptModule();
-
   const allEpisodes = context.feature?.attributes.seasons.reduce((result, currentSeason) => {
-    result.push(new inquirer.Separator(`Season ${currentSeason.season_number}`));
     result.push(
       ...currentSeason.episodes.map((episode) => ({
-        name: `Ep ${episode.episode_number} - ${episode.title}`,
+        title: `[S${formatNumber(currentSeason.season_number)}E${formatNumber(episode.episode_number)}] ${
+          episode.title
+        }`,
         value: episode.feature_id,
       })),
     );
-    return result;
-  }, [] as any);
 
-  const { episodes: selectedEpisodes } = await prompt<{ episodes: Feature['id'][] }>([
-    {
-      type: 'checkbox',
-      name: 'episodes',
-      message: 'Select the episodes you want to download',
-      choices: allEpisodes,
-      pageSize: 30,
-    },
-  ]);
+    return result;
+  }, [] as Choice[]);
+
+  const { episodes: selectedEpisodes } = await prompts({
+    type: 'autocompleteMultiselect',
+    name: 'episodes',
+    message: 'Select the episodes you want to download',
+    choices: allEpisodes,
+  });
 
   return selectedEpisodes;
+}
+
+function formatNumber(number: number) {
+  return number.toString().padStart(2, '0');
 }
