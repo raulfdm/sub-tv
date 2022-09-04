64b15dbb7c6d9948600ae1924037700d6ad29a87
Raul Melo <melo.raulf@gmail.com>
2022-09-04 17:39:25 +0200

    refactor: subtitles module to use prompts

--- apps/cli/src/modules/subtitles.ts
+++ apps/cli/src/modules/subtitles.ts
@@ -1,29 +1,25 @@
+import prompts from 'prompts';
+
 import { apiClient } from '../config/apiClient';
 import { db } from '../config/db';
-import { createPromptModule } from '../config/inquirer';
 import type { SubTvMachineContext } from '../types/main';
 
 export async function subtitlesPrompt(context: SubTvMachineContext): Promise<string[]> {
-  const prompt = createPromptModule();
-
   const subtitlesToDownload: string[] = [];
 
   for await (const featureId of context.featureIdsToSearchFor) {
     const subtitles = await apiClient.searchSubtitle(featureId, db.preferredLanguages);
 
-    const { subtitle } = await prompt<{ subtitle: string[] }>([
-      {
-        name: 'subtitle',
-        type: 'checkbox',
-        message: `Select the subtitle you want to download.`,
-        choices: subtitles.map((subtitle) => ({
-          name: `(${subtitle.attributes.language} - ${subtitle.attributes.download_count} downloads) ${subtitle.attributes.release}`,
-          value: subtitle.attributes.files[0].file_id,
-        })),
+    const { subtitle } = await prompts({
+      name: 'subtitle',
+      type: 'multiselect',
+      message: `Select the subtitle you want to download.`,
+      choices: subtitles.map((subtitle) => ({
+        title: `[${subtitle.attributes.language}] ${subtitle.attributes.release}`,
+        value: subtitle.attributes.files[0].file_id,
+      })),
+    });
 
-        pageSize: 30,
-      },
-    ]);
     subtitlesToDownload.push(...subtitle);
   }
 
